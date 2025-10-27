0ac89872f06a4782caf0a0db18af219f60e3f5a8
Pradeep <deeppradeep1998@gmail.com>
2025-10-27 10:57:25 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+j=4
+while (j):
+    j=j-1
+    print(j,end='|')
+
+#output: 3|2|1|0|   
+#Explanation: The while loop continues as long as j is non-zero. 
+# In each iteration, j is decremented by 1 and then printed,
+#resulting in the output sequence 3, 2, 1, 0 separated by '|'.
